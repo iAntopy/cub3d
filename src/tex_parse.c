fd60292b69142ac08a264b2df8d6d3542d5a63fb
GHebert42 <hebertgermain.qc@gmail.com>
2023-03-12 23:08:21 -0400

    grouille

--- src/tex_parse.c
+++ src/tex_parse.c
@@ -6,78 +6,14 @@
 /*   By: gehebert <gehebert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 08:03:53 by gehebert          #+#    #+#             */
-/*   Updated: 2023/03/12 22:57:06 by gehebert         ###   ########.fr       */
+/*   Updated: 2023/03/12 23:07:30 by gehebert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "cub3d.h"
+#include "../include/cub3d.h"
 // #include "../include/textur.h"
 
-	//      // tex_pars
-			// arg[0] = tex_name char* { "W","N","E","S","F","C"} (upper || lower)
-			// arg[1] = char_case: tex_path  (*char must begin by "./")
-			// arg[1] = num_case : color (get RGB bit_shift << R[255],G[255],B[255])
-			// rules        : chk one or more empty_line_separator
-			//              : orderless mapping info -> attr name/value
-
-
-	/// name_chk :: ft_strfcmp(arg[x], tex_name, tex_name_len);
-
-	//        /// path_chk :: (start: ".\" , end: != "\") 
-// static char	*path_from_line(int start, char const *line)
-	// 	{
-	// 			int		start_def;
-	// 			int		end;
-	// 			char	*path;
-
-	// 			start_def = start;
-	// 			if (!line)
-	// 				return (NULL);
-	// 			while (line[start] && line[start] == ' ')
-	// 				start++;
-	// 			end = ft_strlen(line);
-	// 			while (line[end] == ' ')
-	// 				end--;
-	// 			if (start == start_def || end - start <= 0
-	// 				|| !(path = ft_substr(line, start, end - start)))
-	// 				return (NULL);
-	// 			return (path);
-		
-				// int	i;
-				// int	color;
-				// int	tmp;
-// }
-
-/// color_to_int ...
-
- 					// int	i;
- 					// int	color;
-					// int	tmp;
-
-				// i = 0;
-				// color = 0;
-				// while (str)
-				// {
-				// 	// tmp = ft_atoi(str->content);
-				// 	if (tmp < 0 || tmp > 255)
-				// 		return (-1);
-				// 	color = color | (tmp << (16 - (i++ * 8)));
-				// 	str = str->next;
-				// }
-// }
-					// i = 0;
-					// color = 0;
-					// while (str)
-					// {
-					// 	tmp = ft_atoi(str->content);
-					// 	if (tmp < 0 || tmp > 255)
-					// 		return (-1);
-					// 	color = color | (tmp << (16 - (i++ * 8)));
-					// 	str = str->next;
-// 					// }
-
-
-
+	
 /// color_to_int ...
 int 	str_to_color(int r, int g, int b, int t)
 {
@@ -125,36 +61,7 @@ unsigned char get_ub(int trgb)
 {
 	return (((unsigned char *)&trgb)[0]);
 }
-	/// should get file info about tex_color (sky/floor)
-// int 	parse_color(t_config *config, int key, char const *line)
-		// {
-		// 	int			i;
-		// 	unsigned	color;
-		// 	t_str		*str[2];
-
-		// 	i = 1;
-		// 	while (line[i])
-		// 		if (!ft_in_set(line[i++], " ,0123456789"))
-		// 			return (0);
-		// 	str[0] = NULL;
-		// 	str[1] = NULL;
-		// 	if (!(str[0] = ft_split(line, ' ')) || str_length(str[0]) != 2
-		// 		|| !(str[1] = ft_split(str[0]->next->content, ','))
-		// 		|| str_length(str[1]) != 3)
-		// 		return (str_clear(&str[0]) || str_clear(&str[1]));
-		// 	if ((int)((color = str_to_color(str[1]))) < 0)
-		// 		return (str_clear(&str[0]) || str_clear(&str[1]));
-		// 	config->c[(key == C_F) ? TEX_FLOOR : TEX_SKY] = color;
-		// 	return ((str_clear(&str[0]) || str_clear(&str[1])) | 1);
-// }
-		//chk if arg = 12
-		// txtr_name + path * 4 = 8
-		// txtr_color_name + color_id * 2 = 4
-
-	/// for each: chk_name ref AND chk_path
-		//		if (yes && yes) attib name to path ... 
-		//		also if (name is color_name) : str_to_color
-
+	
 
 static int	setup_wall_textures(t_cub *cub)
 {
