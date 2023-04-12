f4c7edd9f14156e41dcff3e7ac51566f3a5b3da8
GHebert42 <hebertgermain.qc@gmail.com>
2023-04-11 21:05:05 -0400

    first part norminetted >> color_file

--- src/tex_parse.c
+++ src/tex_parse.c
@@ -11,19 +11,12 @@
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
+
 // #include "../include/textur.h"
 
 t_cub	*get_tex_by_id(t_cub *cub, int id, char *tex)
 {
-	// unsigned	color;
-	// char		*str[2];
-	// int			i;
-
-	// i = 1;
-
-	// str[0] = tex;
 	printf("______ HERE GET_BY_ID__[%d]___name{%s}\n", id, &tex[0]);
-
 	if (id == 0)
 		cub->tex.walls[W_SIDE] = mlx_load_png(&tex[0]);
 	else if (id == 1)
@@ -41,24 +34,23 @@ t_cub	*get_tex_by_id(t_cub *cub, int id, char *tex)
 
 static int	setup_wall_textures(t_cub *cub)
 {
-	char		**src;
+	char			**src;
 	mlx_texture_t	**dst;
-//	int		i;
 
 	src = cub->tex.tex_n;
 	dst = cub->tex.walls;
 	if (src[W_SIDE] && src[N_SIDE] && src[E_SIDE] && src[S_SIDE])
 	{
-		printf("All textures available :  \n- %s- %s- %s- %s\n", src[W_SIDE], src[N_SIDE], src[E_SIDE], src[S_SIDE]);
+		printf("All textures available :  \n- %s- %s- %s- %s\n", src[W_SIDE],
+				src[N_SIDE], src[E_SIDE], src[S_SIDE]);
 		dst[W_SIDE] = mlx_load_png(src[W_SIDE]);
 		dst[N_SIDE] = mlx_load_png(src[N_SIDE]);
 		dst[E_SIDE] = mlx_load_png(src[E_SIDE]);
 		dst[S_SIDE] = mlx_load_png(src[S_SIDE]);
 	}
-	return (cub->tex.walls[W_SIDE] && cub->tex.walls[N_SIDE] 
+	return (cub->tex.walls[W_SIDE] && cub->tex.walls[N_SIDE]
 		&& cub->tex.walls[E_SIDE] && cub->tex.walls[S_SIDE]);
 }
-
 
 static int	error_clear(char *err, t_map *map, char ***txtr)
 {
@@ -66,17 +58,17 @@ static int	error_clear(char *err, t_map *map, char ***txtr)
 		strtab_clear(txtr);
 	return (error(err, map));
 }
+
 /// get input frm file
 int	tex_parse(t_cub *cub, t_map *map, int fd)
 {
-	char **txtr;
-	char *line;
+	char	**txtr;
+	char	*line;
 	char	**color;
-	int nb;
-	int id;
-	
-	
-	printf("DEBUG:Z. JOURNEY'S INTO TEX_PARSE\n");	
+	int		nb;
+	int		id;
+
+	printf("DEBUG:Z. JOURNEY'S INTO TEX_PARSE\n");
 	nb = 0;
 	while (nb < 6)
 	{
@@ -88,23 +80,27 @@ int	tex_parse(t_cub *cub, t_map *map, int fd)
 			line = get_next_line(fd);
 		}
 		if (line)
-		{			
+		{
 			map->lines_to_map++;
 			txtr = ft_split(line, ' ');
-			if (txtr[1][ft_strlen(txtr[1])-1] == '\n')
-				txtr[1][ft_strlen(txtr[1])-1] = '\0';
-			if (ft_strlen(txtr[0]) > 2)	
-				return (error_clear("7, Texture mapping Name error !\n", map, &txtr));
-			if (!txtr[1])	
-				return (error_clear("8, Texture mapping Path error !\n", map, &txtr));
+			if (txtr[1][ft_strlen(txtr[1]) - 1] == '\n')
+				txtr[1][ft_strlen(txtr[1]) - 1] = '\0';
+			if (ft_strlen(txtr[0]) > 2)
+				return (error_clear("7, Texture mapping Name error !\n", map,
+						&txtr));
+			if (!txtr[1])
+				return (error_clear("8, Texture mapping Path error !\n", map,
+						&txtr));
 			id = 0;
 			id = ft_in_set((const char *)txtr[0], (const char *)"WNESCF");
-			if ( id < 0)
-				return (error_clear("9, Texture Name unmatching error !\n", map, &txtr));
+			if (id < 0)
+				return (error_clear("9, Texture Name unmatching error !\n", map,
+						&txtr));
 			else if (id < 4)
 			{
-				cub->tex.tex_n[id] = txtr[1];//
-				printf("DEBUG:  tex_id: %d :: tex_name: %s :: \n", id, cub->tex.tex_n[id]); 
+				cub->tex.tex_n[id] = txtr[1]; //
+				printf("DEBUG:  tex_id: %d :: tex_name: %s :: \n", id,
+						cub->tex.tex_n[id]);
 			}
 			else if (id < 7)
 			{
@@ -113,26 +109,37 @@ int	tex_parse(t_cub *cub, t_map *map, int fd)
 				else
 				{
 					color = ft_split(txtr[1], ',');
-					printf("DEBUG:  ID: %d :: color_num[0]: R = %s :: \n", id, color[0]); 
-					printf("DEBUG:  ID: %d :: color_num[1]: G = %s :: \n", id, color[1]); 
-					printf("DEBUG:  ID: %d :: color_num[2]: B = %s :: \n", id, color[2]); 
+					printf("DEBUG:  ID: %d :: color_num[0]: R = %s :: \n", id,
+							color[0]);
+					printf("DEBUG:  ID: %d :: color_num[1]: G = %s :: \n", id,
+							color[1]);
+					printf("DEBUG:  ID: %d :: color_num[2]: B = %s :: \n", id,
+							color[2]);
 					if (id == 4)
-						cub->tex.color[0] = str_to_color(ft_atoi(color[0]), ft_atoi(color[1]),
-							ft_atoi(color[2]), 0xff);
+						cub->tex.color[0] = str_to_color(ft_atoi(color[0]),
+															ft_atoi(color[1]),
+															ft_atoi(color[2]),
+															0xff);
 					else if (id == 5)
-						cub->tex.color[1] = str_to_color(ft_atoi(color[0]), ft_atoi(color[1]),
-							ft_atoi(color[2]), 0xff);
+						cub->tex.color[1] = str_to_color(ft_atoi(color[0]),
+															ft_atoi(color[1]),
+															ft_atoi(color[2]),
+															0xff);
 				}
 			}
 		}
-		else 
+		else
 		{
 			printf("DEBG: UNABLE tex_parse!");
 		}
-		free(line);	
+		free(line);
 		nb++;
 	}
-	printf("DEBUG:Z. JOURNEY'S end TEX_PARSE\n");	
+	printf("DEBUG:Z. JOURNEY'S end TEX_PARSE\n");
 	return (setup_wall_textures(cub));
-//	return (0);
+	//	return (0);
 }
+/**
+ * @brief tex_parse too long split inot 2-3
+ * 
+ */
