b3080b8d71849e52edf02f462686d97234aab5a9
GHebert42 <hebertgermain.qc@gmail.com>
2023-04-27 10:20:28 -0400

    update

--- src/t_ref_name.c
+++ src/t_ref_name.c
@@ -41,8 +41,8 @@ t_cub	*mx_struct(t_map *m, t_cub *cub)
 	return (cub);
 }
 
-t_cub *chsr_feed(t_cub *cub)
-{ 
+t_cub	*chsr_feed(t_cub *cub)
+{
     int i;
     int j;
 
@@ -69,12 +69,12 @@ t_box *e_mtrx_link(t_box *box, char **raw)
     char    *tex_name;
     int i;
 
-    i = 0;
+    i = -1;
     box->pnum = 0;
     box->xform = (mlx_texture_t **)malloc(sizeof(mlx_texture_t *) * box->xnum);
     if(!box->xform )
         return (NULL);    
-    while (i < box->xnum)
+    while (++i < box->xnum)
     {   
         if(raw[i][0] > 32)
         {
@@ -88,57 +88,36 @@ t_box *e_mtrx_link(t_box *box, char **raw)
             if (tex_name[0] == 'z')
                 box->sky = box->xform[i];                
         } 
-        ++i;
-    }
-    return (box); 
+	}
+	return (box);
 }
 
-t_cub *e_mtrx_count(t_cub *cub)
-{ 
-    int i;
-    // int xnum;
+t_cub	*e_mtrx_count(t_cub *cub)
+{
+	int		i;
+	char	*rawz;
 
-    i = -1;
-    cub->box.chrs_len = 1;
-    cub->box.xnum = 0;
-    while (*cub->map.raw && cub->map.raw[++i])    
-    {
-
-        if (cub->map.raw[i][0] > 32 && cub->map.raw[i][0] < 97  && cub->map.raw[i][1] == 32)
-            ++cub->box.chrs_len;
-        /*     find 'png' ended file*/
-        if (ft_strchr_set(cub->map.raw[i], ".png") != NULL) 
-        {
-            printf("COUNT legende [%d] into_Raw {%s} ...\n", i, cub->map.raw[i]); 
-            ++cub->box.xnum;
-        }
-    }
-    // printf("LINE READ from RAW= %d \n", i);
-    return (cub); 
+	i = -1;
+	rawz = cub->map.raw;
+	cub->box.chrs_len = 1;
+	cub->box.xnum = 0;
+	while (*cub->map.raw && cub->map.raw[++i])
+	{
+		if (rawz[i][0] > 32 && rawz[i][0] < 97 && rawz[i][1] == 32)
+			++cub->box.chrs_len;
+		if (ft_strchr_set(rawz[i], ".png") != NULL)
+			++cub->box.xnum;
+	}
+	return (cub);
 }
 
-t_cub  *e_list_txtr(t_cub *cub, t_box *box, t_map *map)
-{ 
-      
-    box->xnum = 0;    //png ended file
-    cub = e_mtrx_count(cub);    // xnum count
-    printf("\nXNUM = %d ", cub->box.xnum);
-    printf("___CHRS_LEN = <%d>\n", cub->box.chrs_len);
-    cub->box = *e_mtrx_link(box, map->raw);  // link legend
-    cub = chsr_feed(cub);       // fill CHRS
-    
-    return (cub); 
+t_cub	*e_list_txtr(t_cub *cub, t_box *box, t_map *map)
+{
+	box->xnum = 0;
+	cub = e_mtrx_count(cub);
+	printf("\nXNUM = %d ", cub->box.xnum);
+	printf("___CHRS_LEN = <%d>\n", cub->box.chrs_len);
+	cub->box = *e_mtrx_link(box, map->raw);
+	cub = chsr_feed(cub);
+	return (cub);
 }
-
-/*  Ref. Tex. By assign.    VER.1
-            get a   txtr_name -> assign it to a char.
-                enum : how many txtr to mapp.
-                dict : bind unsigned char ptr to txtr->path
-                link : load_png_to_txtr ==> ref by unsign_char 
-    
-    Result  :   LowerCase ('a-z')   = txtr. Mapping (path_name for now!)
-            :   UpperCase ('A-Z')   = wall txtr. matrix (tmx)  A = [a,b,c,d] :[West,North,East,Sud]
-            :   Numeric   ('0-9')   = floor casting type { 0 = basic }
-            :   Meta ('<,>,/,?,\,|,=,+,!,@,#,$,%,&') = reserve for object or spec.
-
-*/
