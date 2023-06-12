47c10fb3ef219c03b9892083ce70647e59c6a94c
GHebert42 <hebertgermain.qc@gmail.com>
2023-06-12 19:39:49 -0400

    ressurect to be push on git

--- b_src/t_wall_chk.c
+++ b_src/t_wall_chk.c
@@ -6,7 +6,7 @@
 /*   By: gehebert <gehebert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 20:23:11 by gehebert          #+#    #+#             */
-/*   Updated: 2023/06/12 19:17:03 by gehebert         ###   ########.fr       */
+/*   Updated: 2023/06/12 19:27:37 by gehebert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,6 +61,11 @@ t_objx	*get_pos(t_cub *cub, t_map *m, int o_cells, int id)
 		objx->alleg = m->raw[o_cells][2] - 48;
 		if (objx->o_type == OBJ_PLAYER)
 			objx->alleg = ALI_TORRENT;
+		printf("META_ID[%d]_typ[%d]_name{%c}__",
+					objx->obj_id, objx->o_type, objx->name);
+		printf("Alg[%d]__Rel{%c}__",  objx->alleg, objx->relativ);
+		printf("(x[%d],y[%d])((head:%d))\n\n", objx->opos[0],
+			objx->opos[1], head);	
 	}
 	if (m->pos_x <= 0 || m->pos_y <= 0)
 	{
@@ -70,6 +75,9 @@ t_objx	*get_pos(t_cub *cub, t_map *m, int o_cells, int id)
 	return (objx);
 }
 
+		// printf("get_pos: objx type attribution : head %d\n", head);  
+		// printf("get_pos: objx type attribution : type %d\n", objx->o_type); 
+		
 t_map	*check_hero_found(t_map *m)
 {
 	if (m->pos_x <= 0 || m->pos_y <= 0)
