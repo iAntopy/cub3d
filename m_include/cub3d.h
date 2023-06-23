e6137ad97bc250bbdef78168ccd8cd6a48ee857f
Ian Antony Mongeon Charest <i-antony@hotmail.com>
2023-06-23 19:35:25 -0400

    m_include norminetted

--- m_include/cub3d.h
+++ m_include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: iamongeo <iamongeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 18:18:35 by iamongeo          #+#    #+#             */
-/*   Updated: 2023/04/17 19:44:41 by iamongeo         ###   ########.fr       */
+/*   Updated: 2023/06/23 19:34:57 by iamongeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,15 +69,6 @@ enum	e_sides
 	E_SIDE = 2,
 	S_SIDE = 3
 };
-/*
-/// PARSING ///////////////////
-// coordonees standard
-typedef struct  s_pos
-{
-	int		x;
-	int		y;
-}   t_pos;
-*/
 
 // collision_map : 1D array map where 1 is solid wall otherwise 0.
 // grid_coords : top-left corner coordinate for grid indexed [cell_y][cell_x]
@@ -98,7 +89,6 @@ typedef struct s_map_data
 	char	*collision_map;
 	float	**grid_coords;
 
-	// Germain specific Stuff
 	char	*file;
 	char	**tab;
 	char	**txtr;
@@ -119,59 +109,57 @@ typedef struct s_texture_data
 	mlx_texture_t	*walls[4];
 	char			**rgbx;
 	int				color[2];
-//	char			*tex_n[4];// tex_name
 }	t_tex;
 
 typedef struct s_raycaster_data		t_rcast;
 typedef struct s_cub3d_core_data	t_cub;
 
+// Struct used by raycaster to calculate ray collisions. These structs will be 
+// in an array length SCN_WIDTH. The last block of variables will contain 
+// the results of the collisions for a specific ray. The array
+// cub.hero.rcast.rdata[SCN_WIDTH] will be initialized by ini_raycaster() and
+// updated by update_raycaster(). All this information will be used by renderer
+// functions on draw calls.
 typedef struct s_ray_collision_data
 {
-//	External ref
 	t_rcast	*rcast;
 
-//	Struct constants;
 	int		idx;
-	int		*pcx;//	hero cell position in x
-	int		*pcy;// hero cell position in y
-	float	*px;// hero position x; Pointer to cub->hero.px.
-	float	*py;// hero position y;
-	float	*p_dirx;// player's directional vector in x;
-	float	*p_diry;// player's directional vector in y;
-	float	*rx;// ray vector direction in x for specific ray; Pointer to rcast->rays[0] at rays idx.
-	float	*ry;// ray vector direction in y for specific ray; Pointer to rcast->rays[1] at rays idx.
+	int		*pcx;
+	int		*pcy;
+	float	*px;
+	float	*py;
+	float	*p_dirx;
+	float	*p_diry;
+	float	*rx;
+	float	*ry;
 
-//	Init data
-	int		dx;//	x offset of cell to check in collision map
-	int		dy;//	y offset of cell to check in collision map
-	int		cincr_x;//	direction of cell move for vertical axis collision. Either 1 or -1.
-	int		cincr_y;//	direction of cell move for horizontal axis collision. Either 1 or -1.
-	float	a;//	ray slope
-	float	inv_a;//a inverse == 1/a;
-	float	b;//	ray y offset
+	int		dx;
+	int		dy;
+	int		cincr_x;
+	int		cincr_y;
+	float	a;
+	float	inv_a;
+	float	b;
 
-//	Tracked data (final value is resulting data)
-	int		cx;// tracked collision cell x; Init to hero.cell_x. Final value is collision cell x.
-	int		cy;// tracked collision cell y; Init to hero.cell_y. Final value is collision cell y.
-	
-//	Resulting data
-	int		side;// collision side. Can be compared to side enums.
-	float	hitx;// collision world coord x;
-	float	hity;// collision world coord y;
-	float	dist;// collision distance to projection plane.
-	float	tex_ratio;// ratio of hit on wall from left to right. Used to find drawn texture column.
-	float	tex_height;// texture height on projection screen. Can be greater then SCN_HEIGHT.
-
+	int		cx;
+	int		cy;
+	int		side;
+	float	hitx;
+	float	hity;
+	float	dist;
+	float	tex_ratio;
+	float	tex_height;
 }	t_rdata;
 
 typedef struct s_raycaster_data
 {
 	t_cub		*cub;
 	t_map		*map;
-	t_mtx		*theta_offs;// Angle offsets for each angle from 0. Malloced mtx.
-	t_mtx		*ray_thetas;// Angles for each ray. Malloced mtx.
-	t_mtx		*rays[2];// X, Y part for each ray vector. index 0 are Xs, 1 are Ys Malloced mtx.
-	t_rdata		*rdata;//	malloced array of struct with collision data. len SCR_WIDTH
+	t_mtx		*theta_offs;
+	t_mtx		*ray_thetas;
+	t_mtx		*rays[2];
+	t_rdata		*rdata;
 }	t_rcast;
 
 typedef struct s_main_character_data
@@ -180,14 +168,14 @@ typedef struct s_main_character_data
 	int		cell_y;
 	float	px;
 	float	py;
-	float	ori;//	player orientation in radian
-	float	ori_factor;//	ori / 2pi, updated in cub_player_rotate
-	float	*dirx;	// ptr to rays[0][SCN_WIDTH / 2], the x part of the player's directional vector.
-	float	*diry;	// ptr to rays[1][SCN_WIDTH / 2], the y part of the player's directional vector.
-	float	*fov_lx;// left most fov ray x
-	float	*fov_ly;// left most fov ray y 
-	float	*fov_rx;// right most fov ray x 
-	float	*fov_ry;// right most fov ray y 
+	float	ori;
+	float	ori_factor;
+	float	*dirx;
+	float	*diry;
+	float	*fov_lx;
+	float	*fov_ly;
+	float	*fov_rx;
+	float	*fov_ry;
 	t_rcast	rcast;
 }	t_hero;
 
@@ -196,15 +184,17 @@ typedef struct s_renderer_column_params
 {
 	mlx_image_t		*walls_layer;
 	mlx_texture_t	*tex;
-//	uint32_t		*init_pxls;// strat 2
-	int				half_texh;// strat 1
+	int				half_texh;
 	int				scn_height;
-	int				half_height;// strat 1
+	int				half_height;
 	int				scn_start_y;
 	float			ratio;
-//	int			px_incry;
 }	t_rcol;
 
+// Contains all image buffers on which the renderer functions will draw.
+// bg_layer : for sky color and floor color fills.
+// walls_layer : for renderering walls.
+// requires_update : boolean value indicating the on_update func to redraw scene
 typedef struct s_renderer
 {
 	mlx_image_t	*bg_layer;
@@ -214,25 +204,19 @@ typedef struct s_renderer
 
 typedef struct s_cub3d_core_data
 {
-	/// MLX42 DATA
 	mlx_t			*mlx;
-//	mlx_image_t		*imgz;
 	mlx_image_t		*color;
-//	mlx_texture_t	*texr;
 
 	int				tex_id;
-	/// CONSTANT VALUES ////////////////////////////////////////
-	int				scn_midx;	// mid screen x coordinate
-	int				scn_midy;	// mid screen y coordinate
-	float			inv_cw;		// inverse CELL_WIDTH. precalc const division for optimisation
-	float			inv_sw;		// inverse SCN_WIDTH. precalc const used for skymap rendering.
-	float			inv_two_pi;	// 1 / 2pi;
-	/// FOV AND PROJECTION DATA ///////////////////////////////
-	float			fov;// = fov;// field of view
-	float			hfov;// = fov * 0.5f;// half fov
-	float			near_z;// = (0.5f * (float)SCN_WIDTH) / tanf(cub->hfov);
-	float			near_proj_factor;// = CELL_WIDTH * cub->near_z;
-	/// SUBSECTIONS ////////////////////////////////////////////
+	int				scn_midx;
+	int				scn_midy;
+	float			inv_cw;
+	float			inv_sw;
+	float			inv_two_pi;
+	float			fov;
+	float			hfov;
+	float			near_z;
+	float			near_proj_factor;
 	t_map			map;
 	t_tex			tex;
 	t_hero			hero;
@@ -240,23 +224,17 @@ typedef struct s_cub3d_core_data
 }	t_cub;
 
 /// PARSING ///////////////////
-
-//int	load_map(t_cub *cub, char *map_file);
-int		build_collision_map(t_map *map);
-void	print_collision_map(t_map *map);
-int		build_grid_coords_map(t_map *map);
-void	print_map(t_map *map);
+int				build_collision_map(t_map *map);
+void			print_collision_map(t_map *map);
+int				build_grid_coords_map(t_map *map);
+void			print_map(t_map *map);
 
 /// MAP_CHECKER ///////////////
-//map_parse
 t_map			*init_map(t_map *map);
 int				map_checker(t_cub *cub, t_map *map, char *file);
 int				tex_parse(t_cub *cub, t_map *map);
 t_map			*wall_check(t_map *map);
-//int				color_split(t_map *map, int id);
 char			*skip_file_lines(t_map *map, int fd, int nb_lines);
-//int				is_empty_line(char *line);
-//map_tool
 void			flush_empty_lines(char **raw);
 int				error(char *error, t_map *map);
 int				int_strlen(const char *s);
@@ -300,11 +278,9 @@ int				get_is_cell_within_bounds(t_map *map, int cx, int cy);
 int				init_renderer(t_cub *cub);
 int				renderer_clear(t_cub *cub);
 void			render_walls(t_cub *cub, t_rdata *rd);
-void			mlx_set_color_in_rows(mlx_image_t *img, int start, int end, int col);
+void			mlx_set_color_in_rows(mlx_image_t *im, int st, int ed, int col);
 void			cub_put_pixel(mlx_image_t *img, int x, int y, int col);
 void			clear_image_buffer(mlx_image_t *img);
-
-//void			render_scene(t_cub *cub);
 
 /// CHARACTER CONTROLS ////////
 void			cub_player_rotate(t_cub *cub, float rot);
