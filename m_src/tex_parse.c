8b3591c441bdef39687af4ae4763e26f3d211be5
GHebert42 <hebertgermain.qc@gmail.com>
2023-06-24 18:00:13 -0400

    pre_repaired str_to_color

--- m_src/tex_parse.c
+++ m_src/tex_parse.c
@@ -6,17 +6,39 @@
 /*   By: gehebert <gehebert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 08:03:53 by gehebert          #+#    #+#             */
-/*   Updated: 2023/06/24 14:53:26 by gehebert         ###   ########.fr       */
+/*   Updated: 2023/06/24 17:54:08 by gehebert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+static int	error_clr(char *err, t_map *map)
+{
+	printf("[error_clr] {::%s::} ERROR__ \n",  err);
+	if (map->raw)
+		strtab_clear(&map->raw);
+	if (map->txtr)
+		strtab_clear(&map->txtr);
+	// if (err && err[0])
+	// 	return (error(err, map));
+	return (-1);
+}
+
+static void	flush_split_color(char ***split_color)
+{
+	printf("Flush [error_color] \n");//{::%s::} ERROR__ \n",  **split_color);
+	strtab_clear(split_color);
+		// return (error_clr("Failed to parse a color argument from file", map));
+		// return (error("Failed to parse a color argument from file", map));
+}
+
 static int	error_color(t_map *map, char ***split_color)
 {
-	printf(" REALLY!!! { %s  } ERROR__ \n",  **split_color);
-	// strtab_clear(split_color);
-	return (error("Failed to parse a color argument from file", map));
+	printf("Call-Flush [error_color] \n");
+	flush_split_color(split_color);	// strtab_clear(split_color);
+		// printf("[error_color] {::%s::} ERROR__ \n",  **split_color);
+		// return (error("Failed to parse a color argument from file", map));
+	return (error_clr("Failed to parse: 'error_color'.", map));
 }
 
 static int	colors_are_all_digits(char **split_col)
@@ -25,19 +47,23 @@ static int	colors_are_all_digits(char **split_col)
 
 	while (split_col)
 	{
+		if (!*split_col)
+			break;
+		printf(" color digit ?? INIT_CHECKED  {__[%s]__} ?? \n", *split_col);	
 		s = *split_col - 1;
 		while (*(++s))
 		{
 			if (!ft_isdigit(*s))
 			{
-				printf(" REALLY!!! { %s [%c] } !ft_isdigit ret[0] \n", *split_col, *s);
+				// printf(" REALLY!!! { %s [%c] } !ft_isdigit ret[0] \n", *split_col, *s);
+				printf(" END AT ZERO\n");
 				return (0);
 			}
-			printf(" color digit ?? CHECKED  {__[%s]__} \n", *split_col);	
-			split_col++;
 		}
+		printf(" \tlast checked  ::{__%s__}:: \n", *split_col);
+		split_col++;
 	}
-	printf(" REALLY!!!  C_all_Dgt {__%s__} ret[1]\n", *split_col);
+	printf(" END AT ONE\n");
 	return (1);
 }
 
@@ -48,19 +74,23 @@ static int	color_split(t_map *map, char *col_str, int *ret_col)
 
 	*ret_col = 0;
 	color = ft_split_set(col_str + 1, ", ");
-	printf(" REALLY!!!  Color_split {__%s__} \n", *color);
-	if (strtab_len(color) != 3 || !colors_are_all_digits(color))
+	if (strtab_len(color) != 3)
+		return (error_color(map, &color));
+	if( !colors_are_all_digits(color))
 		return (error_color(map, &color));
 	rgb[0] = ft_atoi(color[0]);
 	rgb[1] = ft_atoi(color[1]);
 	rgb[2] = ft_atoi(color[2]);
+	printf(" rgb << Color_split [__%d,%d,%d__] \n", rgb[0],rgb[1],rgb[2]);
 	if (strtab_len(color) != 3 || (rgb[0] < 0 || rgb[0] > 255)
 		|| (rgb[1] < 0 || rgb[1] > 255) || (rgb[2] < 0 || rgb[2] > 255))
 		return (error_color(map, &color));
 	*ret_col = str_to_color(rgb[0], rgb[1], rgb[2], 0xff);
-	strtab_clear(&color);
+	flush_split_color(&color);	// strtab_clear(&color);
 	return (*ret_col);
 }
+	// printf(" POST ft_atoi  Color_split {__%s__} \n", *color);
+	// printf(" Ret. << Color_split {__%d__} \n", *ret_col);
 
 t_cub	*get_tex_by_id(t_cub *cub, int id, char *tex_str)
 {
@@ -89,21 +119,15 @@ t_cub	*get_tex_by_id(t_cub *cub, int id, char *tex_str)
 	return (cub);
 }
 
-static int	error_clr(char *err, t_map *map)
-{
-	strtab_clear(&map->raw);
-	strtab_clear(&map->txtr);
-	if (err && err[0])
-		return (error(err, map));
-	return (-1);
-}
 
 int	tex_parse(t_cub *cub, t_map *map)
 {
 	int		nb;
 	int		id;
+	int clr_chk;
 
 	nb = 0;
+	clr_chk = 0;
 	while (nb < 6 && map->raw[nb])
 	{
 		id = ft_in_set(map->raw[nb][0], (const char *)"WNESCF");
@@ -111,10 +135,16 @@ int	tex_parse(t_cub *cub, t_map *map)
 		if (id < 0 || map->raw[nb][1] != ' ')
 			return (error_clr("Invalid config label found!\n", map));
 		else if (id < 4 && !get_tex_by_id(cub, id, map->raw[nb]))
-			return (error_clr(NULL, map));
+			return (error_clr(map->raw[nb], map));
 		else if (id == 4 || id == 5)
-			if (color_split(map, map->raw[nb], cub->tex.color + (id - 4)) < 0)
-				return (-1);
+		{
+			clr_chk = color_split(map, map->raw[nb], cub->tex.color + (id - 4));
+			printf(" T_parse: ID[%d]:: Color_split >> Ret.{__%d__}  <<\n", id, clr_chk);
+			printf(" T_parse: Raw[%d]::{_%s_}  << >>\n", nb, map->raw[nb]);//, cub->tex.color + (id - 4));
+			
+			if ( clr_chk < 0)
+				return (-1);	
+		}			
 		nb++;
 	}
 	if (cub->tex_id != 3)
